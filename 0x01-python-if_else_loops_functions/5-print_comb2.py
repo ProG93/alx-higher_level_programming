54d2f90daaf81da86d5fdf5f9dd207b36703a418
Progress <progress.goga@gmail.com>
2023-05-09 10:36:18 -0700

    comb2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for num in range(99):
-    print("{:02d}".format(num), end=","
-print("{:d}".format(99))
+for num in range(0, 100):
+    if num == 99:
+        print("{:02d}".format(num))
+    else:
+        print("{:2d}".format(num), end=", ")
