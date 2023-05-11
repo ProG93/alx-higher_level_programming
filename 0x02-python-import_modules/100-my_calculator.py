98b37ef18b33fabe70fcccf0cdda8d779c3e6207
Progress <progress.goga@gmail.com>
2023-05-11 07:59:42 -0700

    correc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,13 +4,13 @@ from sys import argv
 
 if __name__ == "__main__":
     length = len(agv)
-    if length != 3:
+    if length != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    if length == 3:
-        operator = argv[1]
-        a = int(argv[0])
-        b = int(argv[2])
+    if length == 4:
+        operator = argv[2]
+        a = int(argv[1])
+        b = int(argv[3])
         if operator == "+":
             print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
         elif operator == "-":
