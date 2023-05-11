239b927545fc751207a146579203c40e82b7ea67
Progress <progress.goga@gmail.com>
2023-05-11 03:20:34 -0700

    python import

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if _name_ == "_main_":
+    import sys
+
+    num = len(sys.argv)
+    arg = num - 1
+
+    if arg == 0:
+        print("0 arguments.")
+    elif arg == 1:
+        print("1 argument: {:s}".format(sys.argv[1]))
+    else:
+        print("{:d} arguments:".format(arg))
+        for i in range(1, num):
+            print("{:d}: {:s}".format(i, sys.argv[i]))
