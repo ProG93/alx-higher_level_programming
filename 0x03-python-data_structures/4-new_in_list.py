38da41eb87f1c9d415ad817301709dc58505b253
Progress <progress.goga@gmail.com>
2023-05-15 07:22:59 -0700

    correc

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     item = len(my_list) - 1
 
-    if item < 0 or idx > item:
+    if idx < 0 or idx > item:
         return my_list
     new_list = my_list.copy()
     new_list[idx] = element
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for mat in matrix:
         for rix in mat:
             if rix == mat[-1]:
-                print("{:d}".format(rix), end='')
+                print("{:d}".format(rix), end="")
             else:
-                print("{:d}".format(rix), end='')
+                print("{:d}".format(rix), end=" ")
             print('')
