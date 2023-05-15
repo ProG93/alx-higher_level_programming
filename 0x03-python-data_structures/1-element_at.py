265b306a949d6462afe5e7fbd175602141e8e019
Progress <progress.goga@gmail.com>
2023-05-15 07:07:28 -0700

    python3

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 def element_at(my_list, idx):
     item = len(my_list) - 1
 
-    if idx < 1 or idx > item:
+    if idx < 0 or idx > item:
         return None
     else:
         return my_list[idx]
--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,7 +2,7 @@
 def replace_in_list(my_list, idx, element):
     item = len(my_list) - 1
 
-    if item < 0 and idx > item:
+    if idx < 0 or idx > item:
         return my_list
     else:
         my_list[idx] = element
