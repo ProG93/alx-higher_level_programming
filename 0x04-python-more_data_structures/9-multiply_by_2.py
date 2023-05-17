e9291d8a9f644bb3eaa7efaf8fb2dd0facf72b26
Progress <progress.goga@gmail.com>
2023-05-17 07:36:42 -0700

    simple multiply

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,8 @@
  #!/usr/bin/python3
  def multiply_by_2(a_dictionary):
-     new = {}
-    for key, value in a_dictionary.items():
-        new[key] = value * 2
+     new = a_dictionary.copy()
+     list_keys = list(new.keys())
+
+    for key in list_keys:
+        new[key] *= 2
     return new
