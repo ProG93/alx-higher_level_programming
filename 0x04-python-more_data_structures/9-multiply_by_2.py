3229cfbd5348011639362c85acfc77566e04e3c5
Progress <progress.goga@gmail.com>
2023-05-17 07:43:35 -0700

    Mul

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,9 +1,8 @@
  #!/usr/bin/python3
  def multiply_by_2(a_dictionary):
      new = a_dictionary.copy()
-     list_keys = list(new.keys())
 
-    for i in list_keys:
-        new[i] *= 2
+    for i in new.keys():
+        new[i] = new[i] * 2
     
     return (new)
